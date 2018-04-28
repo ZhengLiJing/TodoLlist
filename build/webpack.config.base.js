cee7c98bfd0c3b92ad491b504b9547bcc629f993
jingke <987906521@qq.com>
2018-04-28 21:37:04 +0800

    webpack4 upadate1

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,53 +1,51 @@
-const path = require('path');
-const createVueLoaderConfig = require('./vue-loader.config.js');
-const isDev = process.env.NODE_ENV === "development";
-
-
+const path = require('path')
+const createVueLoaderConfig = require('./vue-loader.config.js')
+const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
-    target: "web",
-    entry: path.resolve(__dirname, '../client/index.js'),
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        filename: 'build-[hash:8].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(vue|js|jsx)$/,
-                loader: 'eslint-loader',
-                enforce: 'pre',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: createVueLoaderConfig(isDev)
-            },
-            {
-                test: /\.jsx$/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: path.resolve(__dirname, 'node_modules')
-            },
-            {
-                test: /\.(png|jpe?g|svg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 1024,
-                            name: 'resources/[path][name]-[hash:8].[ext]'
-                            // name: '[name]-out.[ext]'
-                        }
-                    }
-                ]
+  target: 'web',
+  entry: path.resolve(__dirname, '../client/index.js'),
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'build-[hash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(vue|js|jsx)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: createVueLoaderConfig(isDev)
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: path.resolve(__dirname, 'node_modules')
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'resources/[path][name]-[hash:8].[ext]'
+              // name: '[name]-out.[ext]'
             }
+          }
         ]
-    }
-};
+      }
+    ]
+  }
+}
 
-module.exports = config;
+module.exports = config
--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,109 +1,104 @@
 
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractPlugin = require('extract-text-webpack-plugin');
-const baseConfig = require('./webpack.config.base.js');
-const merge = require('webpack-merge');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const baseConfig = require('./webpack.config.base.js')
+const merge = require('webpack-merge')
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development'
 
 const defaultPlugin = [
-    new webpack.DefinePlugin({
-        'process.env': {
-            NODE_ENV: isDev ? '"development"' : '"production"'
-        }
-    }),
-    new HtmlWebpackPlugin()
-];
-
-let pathsToClean = [
-    'dist',
-  ]
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: isDev ? '"development"' : '"production"'
+    }
+  }),
+  new HtmlWebpackPlugin()
+]
 
 const devServer = {
-    port: 8001,
-    host: '0.0.0.0',
-    overlay: {
-        errors: true
-    },
-    open: true,
-    hot: true
-};
-
-let config;
-
-if(isDev) {
-    config = merge(baseConfig, {
-        module: {
-            rules: [
-                {
-                    test: /\.styl$/,
-                    use: [
-                        'style-loader',
-                        'css-loader',
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        },
-                        'stylus-loader'
-                    ]
-                }
-            ]
-        },
-        devtool: '#cheap-module-eval-source-map',
-        devServer,
-        plugins: defaultPlugin.concat([
-            new webpack.HotModuleReplacementPlugin(),
-            new webpack.NoEmitOnErrorsPlugin(),
-            // new CleanWebpackPlugin(path.resolve(__dirname, 'dist'))
-        ])
-    })
-} else {
-    config = merge(baseConfig, {
-        entry: {
-            app: path.resolve(__dirname, '../client/index.js'),
-            vendor: ['vue']
-        },
-        output: {
-            filename: '[name].[chunkhash:5].js'
-        },
-        module: {      
-            rules: [
-                {
-                    test: /\.styl/,
-                    use: ExtractPlugin.extract({
-                        fallback: 'style-loader',
-                        use: [
-                            'css-loader',
-                            {
-                                loader: 'postcss-loader',
-                                options: {
-                                    sourceMap: true
-                                }
-                            },
-                            'stylus-loader'
-                        ]
-                    })
-                }
-            ]
-        },
-        plugins: defaultPlugin.concat([
-            new ExtractPlugin({
-                filename: 'style.[contentHash:8].css'
-            }),
-            // new CleanWebpackPlugin(pathsToClean),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor'
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'runtime'
-            })
-        ])
-    })
+  port: 8001,
+  host: '0.0.0.0',
+  overlay: {
+    errors: true
+  },
+  open: true,
+  hot: true
 }
 
-module.exports = config;
+let config
+
+if (isDev) {
+  config = merge(baseConfig, {
+    module: {
+      rules: [
+        {
+          test: /\.styl$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            'stylus-loader'
+          ]
+        }
+      ]
+    },
+    devtool: '#cheap-module-eval-source-map',
+    devServer,
+    plugins: defaultPlugin.concat([
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+      // new CleanWebpackPlugin(path.resolve(__dirname, 'dist'))
+    ])
+  })
+} else {
+  config = merge(baseConfig, {
+    entry: {
+      app: path.resolve(__dirname, '../client/index.js'),
+      vendor: ['vue']
+    },
+    output: {
+      filename: '[name].[chunkhash:5].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              'stylus-loader'
+            ]
+          })
+        }
+      ]
+    },
+    plugins: defaultPlugin.concat([
+      new ExtractPlugin({
+        filename: 'style.[contentHash:8].css'
+      }),
+      // new CleanWebpackPlugin(pathsToClean),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'runtime'
+      })
+    ])
+  })
+}
+
+module.exports = config
